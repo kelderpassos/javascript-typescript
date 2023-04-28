f2b3182dba7d93a7f8fc16e0f51ab9005ab3bfb4
kelderpassos <kelder.passos@gmail.com>
2023-04-28 20:34:39 -0300

    Fix: correct wrong returns

--- Back-end/to-do-app/src/interfaces/crud.interface.ts
+++ Back-end/to-do-app/src/interfaces/crud.interface.ts
@@ -1,8 +1,8 @@
-interface Create<I, O> { create(obj: I): Promise<O> | null}
+interface Create<I, O> { create: (obj: I) => Promise<O | null> }
 interface ReadAll<O> { readAll(): Promise<O[]>}
-interface ReadOne<I, O> { readOne(id: string): Promise<O> | null}
-interface Update<I, O> { update(id: string, obj: Partial<I>): Promise<O> | null}
-interface Delete<I, O> { delete(id: string): Promise<O> | null}
+interface ReadOne<I, O> { readOne(id: string): Promise<O | null> }
+interface Update<I, O> { update(id: string, obj: Partial<I>): Promise<O | null> }
+interface Delete<I, O> { delete(id: string): Promise<O | null> }
 
 export interface CrudMethods<T> extends
   Create<T, T>,
