d0032fb2ae3e97bb1e131a2febede73cf26b0d4b
kelderpassos <kelder.passos@gmail.com>
2023-04-29 19:02:31 -0300

    Chore: delete old project

--- Back-end/to-do-app/src/interfaces/layers.interface.ts
+++
@@ -1,6 +0,0 @@
-import { CrudMethods } from "./crud.interface";
-import { RequestHandlers } from "./handlers.interface";
-
-export interface ModelInterface<T> extends CrudMethods<T> {}
-export interface ServiceInterface<T> extends CrudMethods<T> {}
-export interface ControllerInterface extends RequestHandlers {}
