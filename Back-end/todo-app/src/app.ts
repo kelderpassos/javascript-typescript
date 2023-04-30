66569dc767ceb8aaee4d6fb51a4ed10b28cb7256
kelderpassos <kelder.passos@gmail.com>
2023-04-30 20:45:35 -0300

    Chore: change path of the file

--- Back-end/todo-app/src/app.ts
+++ Back-end/todo-app/src/app.ts
@@ -1,11 +1,11 @@
 import express from 'express'
 import cors from 'cors'
-// import { taskRoute } from "./routes/TaskRoute";
-import 'dotenv/config'
+import { route } from './routes/task'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(route)
 
 
